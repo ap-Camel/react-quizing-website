0d2a9121f8d1b647f905471042cd73c9e62f3659
ap-Camel <ayman.abdwlla@gmail.com>
2022-08-14 23:34:53 +0200

    added api call to get quiz data when starting a quiz

--- src/componants/singleUse/quiz/QuizPage.js
+++ src/componants/singleUse/quiz/QuizPage.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { useParams } from 'react-router-dom'
+
+import UseApi from '../../../helpers/UseApi';
 
 function QuizPage() {
+
+    const {id} = useParams();
+
+    const [quizData, setQuizData] = React.useState("");
+
+    React.useEffect(() => {
+        UseApi(`https://localhost:7295/quiz/${id}`, "GET", null, (res) => {
+            setQuizData(res);
+        })
+    }, []);
+
+    if(quizData === "") {
+        return(
+            <div>
+                wait
+            </div>
+        );
+    }
+
     return (
         <div>
-
+            {
+                quizData.examID
+            }
         </div>
     );
 }
